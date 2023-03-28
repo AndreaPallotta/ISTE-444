7a3d787ab6483f07d08ae4f37820f426b2ddb850
AndreaPallotta <andreapallotta.dev@gmail.com>
2023-03-28 00:50:17 -0400

    Fixed serialized enum

---
+++ project2/server/src/api_requests.rs
@@ -0,0 +1,44 @@
+use axum::{http::StatusCode, Json};
+use schemars::JsonSchema;
+use serde::{Serialize, Deserialize};
+use crate::models::User;
+
+#[derive(Deserialize, Serialize, JsonSchema)]
+pub struct ErrorResponse {
+    pub error_msg: &'static str,
+}
+
+#[derive(Serialize)]
+#[serde(rename_all = "snake_case", tag = "result", content = "content")]
+pub enum ApiResponse<T> {
+    Success(T),
+    Error(ErrorResponse),
+}
+
+#[derive(Deserialize)]
+pub struct LoginParams {
+    email: String,
+    password: String,
+}
+
+pub async fn handle_login(Json(payload): Json<LoginParams>) -> (StatusCode, Json<ApiResponse<User>>) {
+    let email: String = payload.email;
+    let password: String = payload.password;
+
+    let password_match: &str = "Password.1";
+    let email_match: &str = "ap4534@rit.edu";
+
+    if password == password_match && email == email_match {
+        let user: User = User {
+            user_id: "1234".to_string(),
+            first_name: "Andrea".to_string(),
+            last_name: "Pallotta".to_string(),
+            email: "ap4534@rit.edu".to_string(),
+            password: "Password".to_string()
+        };
+        (StatusCode::OK, Json(ApiResponse::Success(user)))
+    } else {
+        let error: ErrorResponse = ErrorResponse { error_msg: "Error" };
+        (StatusCode::NOT_FOUND, Json(ApiResponse::Error(error)))
+    }
+}
--- project2/server/src/main.rs
+++ project2/server/src/main.rs
@@ -1,19 +1,13 @@
-use axum::{
-    routing::get,
-    Router, http::StatusCode, Json,
-};
+use axum::{ routing::post, Router };
 use std::net::SocketAddr;
-
-mod models;
-
-use models::User;
+use server::api_requests::handle_login;
 
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
 
     let app: Router = Router::new()
-        .route("/", get(root));
+        .route("/users", post(handle_login));
 
     let addr: SocketAddr = SocketAddr::from(([0, 0, 0, 0], 3000));
     tracing::debug!("listening on {}", addr);
@@ -21,16 +15,4 @@ async fn main() {
         .serve(app.into_make_service())
         .await
         .unwrap();
-}
-
-async fn root() -> (StatusCode, Json<User>) {
-    let user: User = User {
-        user_id: "1234".to_string(),
-        first_name: "Andrea".to_string(),
-        last_name: "Pallotta".to_string(),
-        email: "ap4534@rit.edu".to_string(),
-        password: "Password".to_string()
-    };
-
-    (StatusCode::OK, Json(user))
 }
